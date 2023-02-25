478d0ecb489d8953e47f8578c4ca545b4510b80e
avreliakotta <111601503+avreliakotta@users.noreply.github.com>
2023-02-25 14:33:27 +0200

    Return back header

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ import Forecast from "./Forecast";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+       <header className="App-header">
         
+      
       <Weather />
       <Forecast />
         
         
 
-        
-      
-      </header>
+      </header> 
+      <footer>
+        <small><a href="https://github.com/avreliakotta/my-app" target="_blanck">Open sourced on GitHub </a>coded  by Nataliia Melnyk</small>
+      </footer>  
+   
     </div>
   );
 }
